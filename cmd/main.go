0ef001bebc771fd1c6b14d02e038db607be2311c
Aleksandr <strtmg@gmail.com>
2024-10-19 12:22:28 +0700

    Logic for telegram

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
-	"time"
 	"wombat/internal/config"
 	"wombat/pkg/daemon"
 )
 
 func main() {
-	conf := config.Config{}
-	daemon.Create(&conf, func() {
-		time.Sleep(1 * time.Second)
-		log.Println("<<-->>")
-		log.Println(conf.Bot.Emoji)
+	conf := &config.Config{}
+	daemon.Create(conf, func() {
+		bot, err := tgbotapi.NewBotAPI(conf.Telegram.Token)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		bot.Debug = true
+
+		log.Printf("Authorized on account %s", bot.Self.UserName)
+
+		u := tgbotapi.NewUpdate(0)
+		u.Timeout = 60
+
+		updates := bot.GetUpdatesChan(u)
+
+		for update := range updates {
+			if update.Message != nil { // If we got a message
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+				msg.ReplyToMessageID = update.Message.MessageID
+
+				bot.Send(msg)
+			}
+		}
 	})
 }
