6fa82f41ab7c7aecb51d550ddcb8beee5bfc16fd
Aleksandr <strtmg@gmail.com>
2024-10-27 18:12:14 +0700

    Refactoring

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -11,36 +11,36 @@ import (
 
 func Create(conf Config, task Task) {
 	ctx, cancel := context.WithCancelCause(context.Background())
+	defer cancel(nil)
 
 	signalChan := make(chan os.Signal, 1)
+
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGHUP)
+	defer signal.Stop(signalChan)
 
-	defer func() {
-		signal.Stop(signalChan)
-		cancel(nil)
-	}()
-
-	go func() {
-		for {
-			select {
-			case s := <-signalChan:
-				switch s {
-				case syscall.SIGHUP:
-					conf.Init(os.Args)
-				case os.Interrupt:
-					cancel(nil)
-					os.Exit(1)
-				}
-			case <-ctx.Done():
-				slog.Info("Done.")
-				os.Exit(1)
-			}
-		}
-	}()
+	go handleSignals(ctx, cancel, conf, signalChan)
 
 	if err := execute(ctx, cancel, conf, task); err != nil {
 		slog.Error(fmt.Sprintf("%s\n", err))
 		os.Exit(1)
+	}
+}
+
+func handleSignals(ctx context.Context, cancel context.CancelCauseFunc, conf Config, sigChan chan os.Signal) {
+	for {
+		select {
+		case s := <-sigChan:
+			switch s {
+			case syscall.SIGHUP:
+				conf.Init(os.Args)
+			case os.Interrupt:
+				cancel(nil)
+				os.Exit(1)
+			}
+		case <-ctx.Done():
+			slog.Info("Done.")
+			os.Exit(1)
+		}
 	}
 }
 
