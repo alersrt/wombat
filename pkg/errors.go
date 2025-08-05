25dfdeb3aa84e9e60bc65624f7d53574ed25e116
Aleksandr <strtmg@gmail.com>
2025-08-05 21:02:41 +0700

    v0.3.0

--- pkg/errors.go
+++ pkg/errors.go
@@ -3,21 +3,18 @@ package pkg
 import (
 	"errors"
 	"fmt"
-	"runtime"
-	"time"
+	"log/slog"
 )
 
 func Catch() {
 	if r := recover(); r != nil {
 		switch v := r.(type) {
-		case LocatedError:
-			fmt.Printf("%v %s:%d %v\n", v.createdTs, v.filename, v.line, v.err)
 		case error:
-			fmt.Printf("%v %v\n", time.Now(), v)
+			slog.Error(fmt.Sprintf("%+v", v))
 		case string:
-			fmt.Printf("%v %v\n", time.Now(), v)
+			slog.Error(fmt.Sprintf("%s", v))
 		default:
-			fmt.Printf("%v %v\n", time.Now(), v)
+			slog.Error(fmt.Sprintf("%+v", v))
 		}
 	}
 }
@@ -25,23 +22,15 @@ func Catch() {
 func CatchWithReturn(err *error) {
 	if r := recover(); r != nil {
 		switch v := r.(type) {
-		case LocatedError:
-			fmt.Printf("%v %s:%d %v\n", v.createdTs, v.filename, v.line, v.err)
-			if err != nil {
-				*err = v.err
-			}
 		case error:
-			fmt.Printf("%v %v\n", time.Now(), v)
 			if err != nil {
 				*err = v
 			}
 		case string:
-			fmt.Printf("%v %v\n", time.Now(), v)
 			if err != nil {
 				*err = errors.New(v)
 			}
 		default:
-			fmt.Printf("%v %v\n", time.Now(), v)
 		}
 	}
 }
@@ -49,23 +38,15 @@ func CatchWithReturn(err *error) {
 func CatchWithReturnAndPost(err *error, post func()) {
 	if r := recover(); r != nil {
 		switch v := r.(type) {
-		case LocatedError:
-			fmt.Printf("%v %s:%d %v\n", v.createdTs, v.filename, v.line, v.err)
-			if err != nil {
-				*err = v.err
-			}
 		case error:
-			fmt.Printf("%v %v\n", time.Now(), v)
 			if err != nil {
 				*err = v
 			}
 		case string:
-			fmt.Printf("%v %v\n", time.Now(), v)
 			if err != nil {
 				*err = errors.New(v)
 			}
 		default:
-			fmt.Printf("%v %v\n", time.Now(), v)
 		}
 
 		if post != nil {
@@ -74,20 +55,8 @@ func CatchWithReturnAndPost(err *error, post func()) {
 	}
 }
 
-type LocatedError struct {
-	createdTs time.Time
-	filename  string
-	line      int
-	err       error
-}
-
-func (l *LocatedError) Error() string {
-	return l.err.Error()
-}
-
 func TryPanic(err error) {
 	if err != nil {
-		_, filename, line, _ := runtime.Caller(1)
-		panic(LocatedError{time.Now(), filename, line, err})
+		panic(err)
 	}
 }
