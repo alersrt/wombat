8899d11630a53cb778bd730d28366e717655fdbb
Aleksandr <strtmg@gmail.com>
2025-08-20 01:05:13 +0700

    v0.6.0

--- pkg/errors.go
+++ pkg/errors.go
@@ -7,19 +7,22 @@ import (
 )
 
 // Wrap wraps errors in order of their declaration.
-func Wrap(errors ...any) error {
+func Wrap(errors ...error) error {
 	var sb strings.Builder
 	if errors == nil || len(errors) == 0 {
 		return nil
 	}
 
 	size := len(errors)
+	args := make([]any, size)
 	for i := 0; i < size; i++ {
 		if i == size-1 {
 			sb.WriteString("%w")
 		} else {
 			sb.WriteString("%w: ")
 		}
+		args[i] = errors[i]
 	}
-	return fmt.Errorf(sb.String(), errors...)
+
+	return fmt.Errorf(sb.String(), args...)
 }
