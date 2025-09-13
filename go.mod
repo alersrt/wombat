dd480d5348f78c6b6be38d881f1d19488a9232b5
Aleksandr <strtmg@gmail.com>
2025-09-13 14:08:38 +0700

    v1.0.0

--- go.mod
+++ go.mod
@@ -3,24 +3,6 @@ module wombat
 go 1.25.1
 
 require (
-	github.com/andygrunwald/go-jira v1.16.0
-	github.com/google/cel-go v0.26.1
 	gopkg.in/yaml.v2 v2.4.0
 	mvdan.cc/sh/v3 v3.12.0
-)
-
-require (
-	cel.dev/expr v0.24.0 // indirect
-	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
-	github.com/fatih/structs v1.1.0 // indirect
-	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
-	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stoewer/go-strcase v1.3.1 // indirect
-	github.com/trivago/tgo v1.0.7 // indirect
-	golang.org/x/exp v0.0.0-20250911091902-df9299821621 // indirect
-	golang.org/x/text v0.29.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20250908214217-97024824d090 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250908214217-97024824d090 // indirect
-	google.golang.org/protobuf v1.36.9 // indirect
 )
---
+++ plugins/cel/plugin.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/common/overloads"
+	"github.com/google/cel-go/common/types"
+	"github.com/google/cel-go/common/types/ref"
+	"github.com/google/cel-go/ext"
+)
+
+const varNameSelf = "self"
+
+type Filter struct {
+	prog cel.Program
+}
+
+func NewFilter(filter string) (*Filter, error) {
+	env, err := cel.NewEnv(
+		cel.Variable(varNameSelf, cel.MapType(cel.StringType, cel.AnyType)),
+		cel.Function(overloads.TypeConvertString, cel.Overload(
+			"map_to_string", []*cel.Type{cel.MapType(cel.StringType, cel.AnyType)}, cel.StringType,
+			cel.UnaryBinding(func(value ref.Val) ref.Val {
+				b, _ := json.Marshal(value.Value())
+				return types.String(b)
+			}),
+		)),
+		cel.OptionalTypes(),
+		ext.Regex(),
+		ext.Strings(),
+		ext.Encoders(),
+		ext.Math(),
+		ext.Sets(),
+		ext.Lists(),
+		ext.TwoVarComprehensions(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("filter: new: %v", err)
+	}
+
+	ast, iss := env.Compile(filter)
+	if iss != nil && iss.Err() != nil {
+		return nil, fmt.Errorf("filter: new: %v", iss.Err())
+	}
+
+	prog, err := env.Program(ast)
+	if err != nil {
+		return nil, fmt.Errorf("filter: new: %v", err)
+	}
+
+	return &Filter{prog: prog}, nil
+}
+
+func (f *Filter) Eval(obj any) (bool, error) {
+	bytes, err := json.Marshal(obj)
+	if err != nil {
+		return false, err
+	}
+
+	data := make(map[string]any)
+	if err := json.Unmarshal(bytes, &data); err != nil {
+		return false, err
+	}
+
+	eval, _, err := f.prog.Eval(map[string]any{varNameSelf: data})
+	if err != nil {
+		return false, fmt.Errorf("filter: eval: %v", err)
+	}
+	return eval.Value().(bool), nil
+}
